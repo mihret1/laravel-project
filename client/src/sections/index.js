284c8e2f7e9040f97e835506937723ce0b397f59
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-11-27 23:30:38 +0300

    start navbar

---
+++ client/src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export { Hero,PopularProduct,Subscribe,SuperQuality,Services,SpecialOffer,Footer,CustomerReviews}
