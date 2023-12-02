76bf1ebe40e5b6aba2e37e9066b224301ab91dbf
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-03 01:13:40 +0300

    shop now button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,7 +3,8 @@ const Button=({label,iconURL})=>{
         <button className=" flex  justify-center items-center 
         gap-2 px-7 py-4 border 
         font-montserrat text-lg
-         leading-none bg-coral-red rounded-full text-white border-coral-red">
+         leading-none bg-coral-red rounded-full
+          text-white border-coral-red">
             {label}
             <img 
               src={iconURL}
