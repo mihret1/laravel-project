7bb749e4c0a25befaf5d2716fed343c1a01c630e
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-03 20:26:22 +0300

    add shoe card

---
+++ client/src/components/ShoeCard.jsx
@@ -0,0 +1,33 @@
+// import React from 'react'
+
+const ShoeCard = ({imgURL,bigShoeImage,changeBigShoeImage}) => {
+    const handleClick=()=>{
+        if(bigShoeImg !==imgURL.bigShoe){
+            changeBigShoeImage(imgURL.bigShoe)
+        }
+    }
+  return (
+    <div className={`border-2 rounded-xl 
+    ${bigShoeImage ===imgURL ? 'border-coral-red'
+    :'border-transparent'} cusror-pointer max-sm:flex-1
+    `}
+     onClick={handleClick}
+    >ShoeCard
+      <div className='flex justify-center items-start bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+        <img 
+          src={imgURL.thumbnail}
+          alt='shoe collection'
+          width={127}
+          height={103}
+          className='object-contain'
+          
+          />
+      </div>
+    </div>
+  
+  
+    )
+}
+
+
+export default ShoeCard
--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
-import {statistics} from '../constant'
+import {shoes, statistics} from '../constant'
+import { bigShoe1 } from "../assets/images"
+import ShoeCard from "../components/ShoeCard"
 const Hero=()=> {
   return (
     <section id="home"
@@ -24,6 +26,27 @@ const Hero=()=> {
                ))}
             </div>
          
+         </div>
+         <div className='relative flex-1 flex
+          justify-center items-center max-xl:py-40 xl:min-h-screen bg-primary
+           bg-hero bg-cover bg-center'>
+          <img 
+            src={bigShoe1}
+            alt='shoe collection'
+            width={610}
+            height={500}
+            className='object-contain relative z-10'
+            />
+            <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+              {shoes.map((shoe   )=>(
+                <div key={shoe}>
+                  <ShoeCard 
+                   imgURL={shoe}
+                   changeBigShoeImage={()=>{}}
+                   bigShoeImg=''/>
+                </div>
+              ))}
+            </div>
          </div>
       </section>
 
