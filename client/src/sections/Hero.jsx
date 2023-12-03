68884bc1d1f078774ab4c9ef55dd8b8f565d9228
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-03 14:50:24 +0300

    start hero section

--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -1,22 +1,32 @@
 import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
+import {statistics} from '../constant'
 const Hero=()=> {
   return (
     <section id="home"
          className="w-full flex xl:flex-row flex-col  justify-center min-h-screen gap-10 max-container">
          <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-           <p>Our Summer Collection</p>
-           <h1>
-             <span>The New Arrivals</span><br />
-             <span>Shebet</span>Shoes
+           <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+           <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
+             <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrivals</span><br />
+             <span className='text-coral-red inline-block mt-3'>Shebet</span>Shoes
            </h1>
-           <p>
+           <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
               Discover stylish Shebet  arrival,quality comfort, and innovation for your active life
            </p>
             <Button label='shop now' iconURL={arrowRight} />
+            <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+               {statistics.map((stat)=>(
+                    <div key={stat.label}>
+                      <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+                      <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
+                    </div>
+               ))}
+            </div>
+         
          </div>
       </section>
-      
+
   )
 }
 
