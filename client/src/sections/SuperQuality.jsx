0cb8fe912e45f77c7072dc2649794e2cd66e8ce9
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-12 13:59:53 +0300

    finish service section

--- client/src/sections/SuperQuality.jsx
+++ client/src/sections/SuperQuality.jsx
@@ -1,51 +1,96 @@
 
-import Button from "../components/Button"
- import shoe8 from '../assets/images/shoe8.svg'
+// import Button from "../components/Button"
+//  import shoe8 from '../assets/images/shoe8.svg'
 
-const SuperQuality=()=> {
-  return (
-    <section id="about-us" 
-             className="flex justify-between 
-                        items-center max-lg:flex-col 
-                        gap-10 w-full max-container">
+// const SuperQuality=()=> {
+//   return (
+//     <section id="about-us" 
+//              className="flex justify-between 
+//                         items-center max-lg:flex-col 
+//                         gap-10 w-full max-container">
 
-      <div className="flex flex-1 flex-col">
+//       <div className="flex flex-1 flex-col">
     
-           <h2 className=' font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
-            we provide you
+//            <h2 className=' font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+//             we provide you
               
-             <span className='text-coral-red'>Super</span>
-             <span className='text-coral-red'>Quality</span>Shoes
+//              <span className='text-coral-red'>Super</span>
+//              <span className='text-coral-red'>Quality</span>Shoes
 
-           </h2>
-           <p className='mt-4 lg:max-w-lg info-text'>
-              Ensuring premium Discover stylish Shebet 
-               arrival,quality comfort, and 
-               innovation for your active life
-               stylish Shebet . stylish Shebet 
-               arrival,quality comfort, and 
-               innovation for your active life
-               arrival,quality comfort, and 
-               innovation for your active life
-           </p>
-           <p className="mt-6 lg:max-w-lg info-text"> 
-              our dedication to detail and exllence unsure ur satisfications
-           </p>
-           <div className="mt-11">
-               <Button label='view detail' />   
+//            </h2>
+//            <p className='mt-4 lg:max-w-lg info-text'>
+//               Ensuring premium Discover stylish Shebet 
+//                arrival,quality comfort, and 
+//                innovation for your active life
+//                stylish Shebet . stylish Shebet 
+//                arrival,quality comfort, and 
+//                innovation for your active life
+//                arrival,quality comfort, and 
+//                innovation for your active life
+//            </p>
+//            <p className="mt-6 lg:max-w-lg info-text"> 
+//               our dedication to detail and exllence unsure ur satisfications
+//            </p>
+//            <div className="mt-11">
+//                <Button label='view detail' />   
 
-           </div>
-               </div>
-               <div className='flex-1 flex justify-center items-center'>
-                   <img 
-                        src={shoe8}
-                        alt='shoe8'
-                        width={570}
-                        height={522}
-                        className='object-contain'/>
-               </div>
+//            </div>
+//                </div>
+//                <div className='flex-1 flex justify-center items-center'>
+//                    <img 
+//                         src={shoe8}
+//                         alt='shoe8'
+//                         width={570}
+//                         height={522}
+//                         className='object-contain'/>
+//                </div>
+//     </section>
+//   )
+// }
+
+// export default SuperQuality
+
+
+import Button from "../components/Button"
+import shoe8 from '../assets/images/shoe8.svg'
+
+
+const SuperQuality = () => {
+  return (
+    <section
+      id='about-us'
+      className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'
+    >
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
+          We Provide You
+          <span className='text-coral-red'> Super </span>
+          <span className='text-coral-red'>Quality </span> Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className='mt-11'>
+          <Button label='View details' />
+        </div>
+      </div>
+
+      <div className='flex-1 flex justify-center items-center'>
+        <img
+          src={shoe8}
+          alt='product detail'
+          width={570}
+          height={522}
+          className='object-contain'
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
