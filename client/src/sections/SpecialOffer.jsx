737fca329b544991352239a483739405dddf4ac2
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-12 16:28:14 +0300

    finish the special offer

--- client/src/sections/SpecialOffer.jsx
+++ client/src/sections/SpecialOffer.jsx
@@ -1,7 +1,41 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
 
 const SpecialOffer=()=> {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img src={offer} width={773} height={687} className='object-contain w-full'/>
+      </div>
+
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
+          We Provide You
+          <span className='text-coral-red'> Special </span>
+         Offer
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Embark premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Navigate dedication to detail and excellence ensures your 
+          satisfaction.dedication to detail and excellence ensures your 
+          satisfaction.dedication to detail and excellence ensures your 
+          satisfaction
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight}/>
+          <Button label='Learn more' 
+                    backgroundColor='bg-white'
+                    borderColor='border-slate-gray'
+                    textColor='text-slate-gray'/>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
