df9c8a88f744631475661ccd41585ad9eff17a64
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-12 16:49:43 +0300

    start customer review section

--- client/src/sections/CustomerReviews.jsx
+++ client/src/sections/CustomerReviews.jsx
@@ -1,7 +1,23 @@
 
 const CustomerReviews=()=> {
   return (
-    <div>CustomerReviews</div>
+    <section className='max-container'>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>
+        what our 
+        <span className='text-coral-red'>Customer</span>
+        Say?
+      </h3>
+      <p className='info-text m-auto mt-4 max-w-lg text-center'>
+        Hear genuine stories from our satisfied customer about thier
+        exceptional experiences with us.
+      </p>
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+          {reviews.map((review)=>(
+            <ReviewCard />
+          ))}
+      </div>
+      
+      </section>
   )
 }
 
