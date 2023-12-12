6dc9b41899fbab72c3e6e8ce1e46939c4252e8aa
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-12 18:53:49 +0300

    subscribe page

--- client/src/sections/Subscribe.jsx
+++ client/src/sections/Subscribe.jsx
@@ -1,7 +1,18 @@
+import Button from "../components/Button"
 
 const Subscribe=()=> {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-between
+        items-center max-lg:flex-col gap-10" id="contact-us" >
+          <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>
+            Sign Up from 
+            <span className='text-coral-red'>updates</span> & Newletter
+          </h3>
+          <div>
+            <Button label='Sign Up' fullwidth/>
+          </div>
+       Subscribe
+    </section>
   )
 }
 
