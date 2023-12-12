1ace9ba07a380be739460acfa9eb3a8f64825a67
mihret1 <90724655+mihret1@users.noreply.github.com>
2023-12-12 17:16:41 +0300

    done the review

--- client/src/sections/CustomerReviews.jsx
+++ client/src/sections/CustomerReviews.jsx
@@ -1,4 +1,7 @@
 
+import ReviewCard from "../components/ReviewCard"
+import {reviews} from '../constant'
+
 const CustomerReviews=()=> {
   return (
     <section className='max-container'>
@@ -13,7 +16,14 @@ const CustomerReviews=()=> {
       </p>
       <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
           {reviews.map((review)=>(
-            <ReviewCard />
+            <ReviewCard 
+                 key={review.customerName}
+                 imgURL={review.imgURL}
+                 customerName={review.customerName}
+                 rating={review.rating}
+                 feedback={review.feedback}
+
+                 />
           ))}
       </div>
       
